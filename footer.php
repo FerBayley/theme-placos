e3d43a00ba6f67be4a1c287da275dd6e2092a616
FerBayley <fernando.bayley@gmail.com>
2013-07-11 04:44:22 -0300

    theme placoss
    Arreglos en footer y estilos

--- footer.php
+++ footer.php
@@ -7,8 +7,12 @@
 			<a href="">
 				<img src="<?php bloginfo('template_directory')?>/img/logo.jpg" alt="Placoss Muebles a medida" />
 			</a>
+
 		</footer>
 		<!-- FOOTER -->
+
+		<p class="textoLegal">Todos los derechos reservados &copy;2013 - Argentina</small></p>
+
 		<div class="clearfix"></div>
 
 	</section> <!-- End of wrapper -->
